5d17f5fbe4ba3e8229a1dfa61c5672181ecbc769
Yellow2392 <sebas.tenorio2311@gmail.com>
2023-06-14 20:50:13 -0500

    Update 2

---
+++ src/Login.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect } from 'react';
+import {  Box, Button, ButtonGroup, Stack, TextField } from "@mui/material";
+import { useState } from "react";
+
+
+
+const Login = () => {
+    const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    async function hacerConsultaHTTP(params) {
+        await fetch("www.google.com");
+    }
+    return () => {
+        hacerConsultaHTTP()
+    };
+  }, []);
+
+  function usernameHandler(event) {
+    console.log(event.target.value);
+    setUsername(event.target.value);
+  }
+
+  const passwordHandler = (event) => {
+    setPassword(event.target.value);
+  };
+
+
+    return (
+        <Box 
+          className="App"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          minHeight="100vh"
+        >
+        <Stack spacing={4} width={200}>
+          <h1>Megaprof</h1>
+          <h2>Login</h2>
+          <h3>User:{username}</h3>
+          
+          <TextField
+            id="outlined-basic"
+            label="User"
+            variant="outlined"
+            onChange={usernameHandler}
+          />
+          <h3>Password:{password}</h3>
+          <TextField
+            id="outlined-basic2"
+            label="Password"
+            variant="outlined"
+            onChange={passwordHandler}
+            type={"password"}
+          />
+  
+          <Button>login</Button>
+          <p>No account yet? Register as:</p>
+          <ButtonGroup variant="contained" aria-label="outlined primary button group">
+  <Button>Student</Button>
+  <Button>Teacher</Button>
+</ButtonGroup>
+        </Stack>
+      </Box>
+    );
+};
+
+export default Login;
