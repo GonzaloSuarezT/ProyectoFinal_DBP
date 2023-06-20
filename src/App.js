43e7ccd1de8b0612ad710fde0bd7997ad92a3d7c
GonzaloSuarezT <74025195+GonzaloSuarezT@users.noreply.github.com>
2023-06-20 14:00:33 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 //import logo from './logo.svg';
 import './App.css';
+
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import Home from "./components/Home";
@@ -8,6 +9,7 @@ import RegisterTeacher from "./components/RegTeacher"
 import Cursos from "./components/cursos"
 import Teacher from "./components/Profesor"
 import Profesores from "./components/profesores"
+import menu_teacher from "./components/menu_teacher"
 
 function App() {
   return (
@@ -20,6 +22,7 @@ function App() {
         <Route path="/teacher" element={<Teacher/>}></Route>
         <Route path="/teachers" element={<Profesores/>}></Route>
         <Route path="/" element={<Login/>}></Route>
+        <Route path="/menuteacher" element={<menu_teacher/>}></Route>
       </Routes>
     </Router>
 
