3a7ba897635d86b87cb77bcaf662739b9b723e3f
Yellow2392 <sebas.tenorio@gmail.com>
2023-07-06 14:30:17 -0500

    Update 16

--- reactProject/src/components/cursos.jsx
+++ reactProject/src/components/cursos.jsx
@@ -1,44 +1,56 @@
-import React, { useEffect } from 'react';
-import { useState } from "react";
+import React, { useEffect, useState } from 'react';
 import {  Box, Button, Stack, TextField , ButtonGroup} from "@mui/material";
-
-/*
-const signatures = [
-    {
-      value: 'Maths',
-      label: 'Maths',
-    },
-    {
-      value: 'Literature',
-      label: 'Literature',
-    },
-    {
-      value: 'History',
-      label: 'History',
-    },
-    {
-      value: 'Chemistry',
-      label: 'Chemistry',
-    },
-    {
-      value: 'Physics',
-      label: 'Physics',
-    },
-    {
-      value: 'Geography',
-      label: 'Geography',
-    }
-  ];
-*/
+import { DataGrid } from '@mui/x-data-grid';
+import axios from "axios";
 
 const Cursos =() =>{
-    const [course, setCourse] = useState("");
+    const [course, setCourse] = useState("Maths");
     function courseHandler(event) {
         console.log(course);
         setCourse(event.target.value);
     }
 
+  const [loadingData, setLoadingData] = useState(true);
+  const [serverData, setServerData] = useState([]);
+  const [rows, setRows] = useState([]);
+  const columns = [
+    { field: "username", headerName: "Teacher", width: 120 },
+    { field: "email", headerName: "Email", width: 170 },
+    { field: "expYears", headerName: "Experience years", width: 150 },
+    { field: "check", headerName: "Choose", width: 70 },
+  ];
     
+  useEffect(() => {
+    async function getData() {
+      await axios
+        .get(`http://127.0.0.1:5000/teachers/${course}`)
+        .then((response) => {
+          setServerData(response.data);
+          fillRows(response.data);
+          setLoadingData(false);
+          
+        })
+    }
+    if (loadingData) {
+      getData();
+    }
+  }, []);
+
+  function fillRows(responseData) {
+    let rowArray = [];
+    for (let count = 0; count < responseData.length; count++) {
+      let row;
+      row = {
+        id: count,
+        username: responseData[count].username,
+        email: responseData[count].email,
+        expYears: responseData[count].expYears + " years",
+        check: "filler",
+      };
+      rowArray.push(row);
+    }
+    setRows(rowArray);
+  }
 
     return(
         <Box 
@@ -48,7 +60,7 @@ const Cursos =() =>{
           alignItems="center"
           minHeight="100vh"
         >
-<Stack spacing={4} width={200}>
+<Stack spacing={4} width={700}>
         <h1>Menu</h1>
         <h2>Choose subject</h2>
 <div>
@@ -69,6 +81,17 @@ const Cursos =() =>{
         </ButtonGroup>
         </div>  
         <h3>Available teachers</h3>
+        <>
+      {loadingData ? (
+        <p>Loading Please Wait...</p>
+      ) : (
+        <DataGrid rows={rows} columns={columns} checkboxSelection />
+      )}
+      {console.log("Server Data:")}
+      {console.log(serverData)}
+      {console.log("Rows:")}
+      {console.log(rows)}
+    </>
         </Stack>
     </Box>
     );
